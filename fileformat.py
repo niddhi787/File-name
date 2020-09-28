faaf7ace7497359bbfcbb89db273dff30bc417b4
niddhi787 <72001315+niddhi787@users.noreply.github.com>
2020-09-28 10:37:43 +0530

    Add files via upload

---
+++ fileformat.py
@@ -0,0 +1,11 @@
+a=input("Enter File Name : ")
+if ".py" in a:
+    print("Python file")
+elif ".txt" in a:
+    print("Text file")
+elif ".exe" in a:
+    print("Executable file")
+elif ".pdf" in a:
+    print("pdf file")
+elif ".ppt" in a:
+    print("Powerpoint file")
